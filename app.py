19c2053bc4cc717dc31d1e794c4850b03b12843d
J007acky <111886827+J007acky@users.noreply.github.com>
2025-05-07 11:20:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,24 +55,9 @@ class InputData(BaseModel):
     nonStriker: int
 
 
-# @app.post("/predict")
-# def predict_score(data: InputData):
-#     # Convert input to numpy array and standardize it
-#     print(data)
-#     runs = int(data.runs)
-#     wickets = int(data.wickets)
-#     overs = float(data.over)
-#     striker = int(data.striker)
-#     nonStriker = int(data.nonStriker)
-#     test_data = np.array([[runs, wickets, overs, striker, nonStriker]])
-#     print(test_data)
-#     test_data = sc.transform(test_data)
-
-#     # Make prediction
-#     prediction = reg.predict(test_data)
-#     print(prediction)
-
-#     return {"predicted_final_score": prediction[0]}
+@app.post("/predict")
+def predict_score(data: InputData):
+    return predict_score(data)
 
 
 @app.post("/predict-dll")
